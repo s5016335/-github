51602d71f8011584bc506a4d88e4a27c1fee99a2
Jian_Cheng <s5016335@gmail.com>
2018-05-07 19:02:48 +0800

    update

---
+++ example/Receiver/app/src/main/java/com/example/jiancheng/receiver/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.jiancheng.receiver;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn,btn2;
+    //private static final String BC_ACTION="org.hualang.alarm.action.BC_ACTION";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn= (Button) findViewById(R.id.btn);
+        btn2= (Button) findViewById(R.id.btn2);
+
+        final AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
+        final Intent intent = new Intent();
+        intent.setAction("org.hualang.alarm.action.BC_ACTION");
+        intent.putExtra("msg", "你该起床了");
+        final PendingIntent pi = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+        final long time = System.currentTimeMillis();
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                am.setRepeating(AlarmManager.RTC_WAKEUP,time,5*1000,pi);
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                am.cancel(pi);
+            }
+        });
+
+
+
+
+
+
+    }
+
+
+}
---
+++ example/Receiver/app/src/main/java/com/example/jiancheng/receiver/ScreenBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package com.example.jiancheng.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class ScreenBroadcastReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String action = intent.getAction();
+        String msg=intent.getStringExtra("msg");
+        Toast.makeText(context, "接收的Intent的Action为：" + action + "\n消息内容是"+msg, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+}
